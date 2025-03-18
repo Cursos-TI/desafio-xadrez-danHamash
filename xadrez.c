65c7faa8f05927c34dddfa8fd52c517492eee510
Carlos Daniel <megadestroier@hotmail.com>
2025-03-18 00:51:39 +0000

    Desafio 01 Tema 03. criando a movimentação do bispo, da torre e da rainha. Usando estruturas de repetição.

--- xadrez.c
+++ xadrez.c
@@ -5,17 +5,42 @@
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
+
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
-
+    
+    int torre = 1, bispo = 1, rainha =1;  //varial de entrada, serve para todos os loops!
+    
     // Implementação de Movimentação do Bispo
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação do Bispo em diagonal.
+    
+    // do-while esta sendo usado para repetir o movimento da torre 5 vezes.
+    do{ 
+    printf("Bispo (%d)  para Cima, Direta!\n",bispo); 
+    bispo++; // incrementa +1 a variavel até a função tornar-se falsa e parar o loop.
+    } while (bispo <=5);  // define o limite de repetições.
+
+
+    printf("\n");//separando as peças para melhor vizualização
 
     // Implementação de Movimentação da Torre
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Torre para a direita.
 
+    // while esta sendo usado para repetir o movimento da torre 5 vezes.
+    while ( torre <=5) { // define o limite de repetições
+    printf("Torre (%d) para a Direita!\n",torre); 
+    torre++; // incrementa +1 a variavel até a função tornar-se falsa.
+    }
+
+    printf("\n");//separando as peças para melhor vizualização
+
     // Implementação de Movimentação da Rainha
     // Sugestão: Utilize uma estrutura de repetição para simular a movimentação da Rainha para a esquerda.
+    // int rainha = 1: informa o inicio da variavel, rainha <=8: define o limite de repetições, i++: incrementa até a variavel se tornar falsa e parar o loop.
+    for (int rainha = 1; rainha <= 8 ; rainha++){    
+        printf("Rainha (%d) para a Esquerda!\n", rainha);
+    }
+
 
     // Nível Aventureiro - Movimentação do Cavalo
     // Sugestão: Utilize loops aninhados para simular a movimentação do Cavalo em L.
